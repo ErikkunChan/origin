41cac81feb566c90bdeef3c805cea7b4044a1738
Eric Chan <erikku.chan.97@gmail.com>
2016-10-18 22:39:12 -0400

    Add edited lab 6

---
+++ lab6-TTS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This is a shell script for lab 6
+# Script for saying the current local time
+
+#variable that includes the date and the string literal
+NOW=$(date +"Ground Control time is %M minutes past %l %p U T C")
+
+echo $NOW
+
+#commmand for generating a TTS file and incorporating the variable
+pico2wave -w TimeTTS.wav "$NOW"
+
+#command for playing back the TTS audio file
+aplay TimeTTS.wav
